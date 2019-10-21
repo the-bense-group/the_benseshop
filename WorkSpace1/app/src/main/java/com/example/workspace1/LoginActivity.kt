80edc5c5dfabf6b61cd52731c13a0f5ddce677e8
samarthvattanachhanonn <48404922+samarthvattanachhanonn@users.noreply.github.com>
2019-10-21 23:50:01 +0700

    2110201902

--- WorkSpace1/app/src/main/java/com/example/workspace1/LoginActivity.kt
+++ WorkSpace1/app/src/main/java/com/example/workspace1/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.workspace1
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workspace1.demo1.Demo1MainActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -13,7 +14,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, Demo1MainActivity::class.java))
         }
 
     }
